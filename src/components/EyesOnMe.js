8298937fd3418c87d035b35e3eb8a2307a611c18
Eric J Wilhite <EricJWilhite@gmail.com>
2020-05-13 11:30:27 -0700

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,27 @@
 // Code EyesOnMe Component Here
+import React, { Component } from "react";
+
+class EyesOnMe extends Component {
+    constructor() {
+        super();
+
+        this.handleFocus = this.focusHandler.bind(this)
+        this.handleBlur = this.blurHandler.bind(this)
+    }
+
+    focusHandler() {
+        console.log('Good!')
+    }
+
+    blurHandler() {
+        console.log('Hey! Eyes on me!')
+    }
+
+    render() {
+        return <button onFocus= {this.handleFocus}
+        onBlur = {this.handleBlur}
+        ></button>
+    }
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,21 @@
-// Code Keypad Component Here
+import React, { Component } from "react";
+
+class Keypad extends Component {
+    constructor() {
+        super();
+
+        this.handleKeyUp = this.keyUpHandler.bind(this)
+    }
+
+    keyUpHandler() {
+        console.log('Entering password...')
+    }
+
+    render() {
+        return < input onKeyUp= { this.handleKeyUp } 
+        type="password" 
+        />
+    } 
+}
+
+export default Keypad;
